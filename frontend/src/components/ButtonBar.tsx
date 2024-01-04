a4939a56fa2a9380f0b3e9098b78dcd0f57b6132
Nils Hartmann <nils@nilshartmann.net>
2024-01-04 10:11:13 +0100

    align left or right of buttons

--- frontend/src/components/ButtonBar.tsx
+++ frontend/src/components/ButtonBar.tsx
@@ -1,6 +1,11 @@
 import { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
-type ButtonBarProps = { children: ReactNode };
-export default function ButtonBar({ children }: ButtonBarProps) {
-  return <div className={"space-x-4"}>{children}</div>;
+type ButtonBarProps = { children: ReactNode; align?: "start" | "end" };
+export default function ButtonBar({
+  children,
+  align = "start",
+}: ButtonBarProps) {
+  const className = twMerge("space-x-4", align === "end" && "flex justify-end");
+  return <div className={className}>{children}</div>;
 }
