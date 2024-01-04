fb5727b5c998b526531bb34c16fc1866d42a1653
Nils Hartmann <nils@nilshartmann.net>
2024-01-04 10:09:20 +0100

    timer opens and closes on hover (sometimes at least)

--- frontend/src/components/Timer.tsx
+++ frontend/src/components/Timer.tsx
@@ -10,13 +10,59 @@ type TimerState = {
   timeRemaining: number;
 };
 
-export function Timer() {
-  const [time, setTime] = useState(0);
+type TimeState = {
+  time: number;
+  hasChanged?: boolean;
+};
 
+// close and open on hover only works so-la-la
+// but should be good enough for demo purposes
+export function Timer() {
+  const [open, setOpen] = useState(false);
+  const [canCloseOnLeave, setCanCloseOnLeave] = useState(true);
+  const [time, setTime] = useState<TimeState>({ time: 0 });
   const [timer, setTimer] = useState<TimerState | null>();
+  const [hasFocus, setHasFocus] = useState(false);
+
+  const handleExpand = (open: boolean) => {
+    if (open) {
+      setOpen(true);
+      return;
+    }
+    if (canCloseOnLeave) {
+      setOpen(false);
+    }
+  };
 
   const onTimeChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setTime(parseInt(e.target.value));
+    const newTime = parseInt(e.target.value);
+    setTime((prevTime) => ({
+      hasChanged: prevTime.time !== newTime && newTime !== 0,
+      time: newTime,
+    }));
+
+    setTimer(null);
+  };
+
+  const couldCloseOnLeave = !time.hasChanged && !timer && !hasFocus;
+
+  const activationProps = {
+    onFocus: () => {
+      setHasFocus(true);
+      setCanCloseOnLeave(false);
+    },
+    onBlur: () => {
+      setHasFocus(false);
+      setCanCloseOnLeave(true);
+    },
+    onMouseEnter: () => {
+      setCanCloseOnLeave(false);
+    },
+    onMouseLeave: () => {
+      if (couldCloseOnLeave) {
+        setCanCloseOnLeave(true);
+      }
+    },
   };
 
   const onStartTimer = () => {
@@ -24,7 +70,11 @@ export function Timer() {
       clearInterval(timer?.timerId);
     }
 
-    if (time === 0) {
+    // when timer is started, both input field and start button are removed
+    // so we can not have focus anymore (even we won't receive onBlur in this case)
+    setHasFocus(false);
+
+    if (time.time === 0) {
       return;
     }
 
@@ -43,7 +93,7 @@ export function Timer() {
 
     setTimer({
       state: "running",
-      timeRemaining: time * 60,
+      timeRemaining: time.time * 60,
       timerId: id,
     });
   };
@@ -54,6 +104,7 @@ export function Timer() {
     }
     clearInterval(timer.timerId);
     setTimer(null);
+    setHasFocus(false);
   };
 
   useEffect(() => {
@@ -81,72 +132,88 @@ export function Timer() {
         ? "finished"
         : "edit";
 
-  const buttonEnabled = time > 0;
+  const buttonEnabled = time.time > 0;
 
   return (
     <div
+      onMouseEnter={() => handleExpand(true)}
+      onMouseLeave={() => handleExpand(false)}
       className={twMerge(
-        "max-w-1/4 flex  items-center rounded bg-white pe-4 ps-4 font-barlow",
-        timerUi === "finished" && "bg-green",
+        "max-w-1/4 flex  items-center rounded bg-white pe-2 ps-2 font-barlow",
+        timer?.state === "finished" && "bg-green",
       )}
     >
-      {timerUi === "finished" && (
-        <div className={"me-2 text-white"}>Finished</div>
-      )}
-      {timerUi === "finished" || (
-        <div className={"me-2 text-gray-500"}>Timer</div>
-      )}
+      {open && (
+        <>
+          {timerUi === "finished" && (
+            <div className={"me-2 text-white"}>Finished</div>
+          )}
+          {timerUi === "finished" || (
+            <div className={"me-2 text-gray-500"}>Timer</div>
+          )}
 
-      {timerUi === "edit" && (
-        <>
-          <div className={"max-w-16"}>
-            <Input
-              name={"time"}
-              type={"number"}
-              value={time}
-              onChange={onTimeChange}
-              min={0}
-              max={300}
-            />
-          </div>
-          <span className={"me-2 ms-1"}>{" min "}</span>
-          <div className={"ms-2"}>
-            <Button disabled={!buttonEnabled}>
-              <button onClick={onStartTimer}>
-                <i className="fa-solid fa-hourglass-start me-2"></i>Start
-              </button>
-            </Button>
-          </div>
+          {timerUi === "edit" && (
+            <>
+              <div className={"max-w-16"}>
+                <Input
+                  name={"time"}
+                  type={"number"}
+                  value={time.time}
+                  onChange={onTimeChange}
+                  min={0}
+                  max={300}
+                  {...activationProps}
+                />
+              </div>
+              <span className={"me-2 ms-1"}>{" min "}</span>
+              <div className={"me-2 ms-2"}>
+                <Button disabled={!buttonEnabled}>
+                  <button
+                    onClick={onStartTimer}
+                    {...activationProps}
+                    name={"startBtn"}
+                  >
+                    Start
+                  </button>
+                </Button>
+              </div>
+            </>
+          )}
+          {timerUi === "running" && (
+            <>
+              <div className={"me-2 w-8 font-bold text-gray-500"}>
+                {formatMinSeconds(timer?.timeRemaining || 0)}
+              </div>
+              <div className={"me-2 ms-2 "}>
+                <Button disabled={!buttonEnabled}>
+                  <button onClick={onCancelTimer} name={"cancelBtn"}>
+                    Cancel
+                  </button>
+                </Button>
+              </div>
+            </>
+          )}
+          {timerUi === "finished" && (
+            <>
+              <div className={"me-2 w-8 font-bold text-white"}>
+                {formatMinSeconds(time.time * 60)}
+              </div>
+              <div className={"me-2 ms-2"}>
+                <Button disabled={!buttonEnabled}>
+                  <button onClick={onCancelTimer} name={"resetBtn"}>
+                    Reset
+                  </button>
+                </Button>
+              </div>
+            </>
+          )}
         </>
       )}
-      {timerUi === "running" && (
-        <>
-          <div className={"me-2 w-8 font-bold text-gray-500"}>
-            {formatMinSeconds(timer?.timeRemaining || 0)}
-          </div>
-          <div className={"ms-2"}>
-            <Button disabled={!buttonEnabled}>
-              <button onClick={onCancelTimer}>
-                <i className="fa-solid fa-xmark me-2"></i>Cancel
-              </button>
-            </Button>
-          </div>
-        </>
-      )}
-      {timerUi === "finished" && (
-        <>
-          <div className={"me-2 w-8 font-bold text-white"}>
-            {formatMinSeconds(time * 60)}
-          </div>
-          <div className={"ms-2"}>
-            <Button disabled={!buttonEnabled}>
-              <button onClick={onCancelTimer}>
-                <i className="fa-solid fa-xmark me-2"></i>Reset
-              </button>
-            </Button>
-          </div>
-        </>
-      )}
+      <Button>
+        <button onClick={() => setOpen((o) => !o)}>
+          <i className="fa-solid fa-hourglass-start"></i>
+        </button>
+      </Button>
     </div>
   );
 }
