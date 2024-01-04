c36378ac5b26f2c6541d286ccf42aa8ae15dd817
Nils Hartmann <nils@nilshartmann.net>
2024-01-04 10:11:25 +0100

    input and textarea

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,19 +1,24 @@
 import IntrinsicElements = React.JSX.IntrinsicElements;
+import { twMerge } from "tailwind-merge";
 
-type InputProps = {
-  value?: string | undefined;
-  placeholder?: string | undefined;
-};
+type InputProps = IntrinsicElements["input"];
 
-type X = IntrinsicElements["input"];
-
-export function Input(props: X) {
-  return (
-    <input
-      className={
-        "h-8 max-w-full rounded border border-gray-400 bg-white p-2 text-gray-500"
-      }
-      {...props}
-    ></input>
+export function Input({ className, ...rest }: InputProps) {
+  const inputClassname = twMerge(
+    "h-8 w-full max-w-full rounded border border-gray-400 bg-white p-2 text-gray-500",
+    className,
   );
+
+  return <input className={inputClassname} {...rest}></input>;
+}
+
+type TextareaProps = IntrinsicElements["textarea"];
+
+export function Textarea({ className, ...rest }: TextareaProps) {
+  const inputClassname = twMerge(
+    "w-full max-w-full rounded border border-gray-400 bg-white p-2 text-gray-500",
+    className,
+  );
+
+  return <textarea className={inputClassname} {...rest}></textarea>;
 }
